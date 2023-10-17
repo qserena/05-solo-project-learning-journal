1ca0ef84b288aafff391b42e55304630fe26bf30
Tom Serenander <tom@serenander.se>
2023-10-17 14:32:35 +0200

    Header and .hero ready for mobile.

--- data.js
+++ data.js
@@ -1,3 +1,7 @@
-export function getArticlePreview() {
+export function getArticlePreviewText() {
 	return `After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.`
+}
+
+export function getPostText() {
+	return `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`
 }
--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-import { getArticlePreview } from './data.js'
+import { getArticlePreviewText, getPostText } from './data.js'
 
 const heroContainer = document.getElementById('hero-container')
 const recentPostsEl = document.getElementById('recent-posts')
 
-function getArticle() {
+function getArticlePreview() {
 	return `
 		<div class="article-preview" id="article-preview">
-			<h1>My new journey as a bootcamp student.</h1>
-			<p>${getArticlePreview()}</p>
+			<p class="article-date-hero">OCT 17, 2023</p>
+			<h2>My new journey as a bootcamp student.</h2>
+			<p class="article-preview-hero">${getArticlePreviewText()}</p>
 		</div>
 	`
 }
 
 function getPosts() {
 	let html = ``
+	const blogNumbers = [`one`, `two`, `three`, `four`, `five`, `six`]
 	for (let i = 0; i < 6; i++) {
 		html += `
 	        <div class="recent-post">
 	            <img src="./img/blog-image-0${i + 1}.png"/>
+				<p class="date">OCT 17, 2023<p/>
+				<h3>Blog ${blogNumbers[i]}</h3>
+				<p>${getPostText()}</p>
 	        </div>
 	    `
 	}
 	return html
 }
 
-console.log(heroContainer)
-
-heroContainer.innerHTML = getArticle()
+heroContainer.innerHTML = getArticlePreview()
 recentPostsEl.innerHTML = getPosts()
